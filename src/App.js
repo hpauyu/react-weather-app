93fbd294cc048aaf9417caafeb18a615af5ccf76
hpauyu <hpauyunanshawng@yahoo.com>
2024-10-16 20:33:14 -0500

    update gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const myId = process.env.REACT_APP_APPID
+  const myURL = process.env.REACT_APP_URL
+
+  const[city, setCity] = useState('')
+  const[data, setData] =  useState({})
+
+  useEffect(()=>{
+
+  },[data])
+
+  const handleClick =()=>{
+    let searchCity = city
+    const fetchData = async()=>{
+      try {
+        const response = await fetch(myURL+searchCity+myId)
+        const result = await response.json()
+        setData({...result})     
+        console.log(result)    
+      }catch(error){
+        console.log(error)
+      }
+    }
+    fetchData();
+    parseObject();
+  }
+
+  const parseObject = ()=>{
+    if (data.cod === 200){
+      console.log(data.weather)
+    }
+    // console.log(data.cod)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <input type='text' onChange={(e)=>setCity(e.target.value)}></input> 
+      <button onClick={handleClick}>Search</button>
+      </div>
     </div>
   );
 }
